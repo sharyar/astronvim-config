440070dc3b98ce62f1720dad8ff904a491bc90de
Sharyar Memon <sharyar@ualberta.ca>
2023-05-30 21:37:10 -0600

    feat: adds obsidian

--- plugins/community.lua
+++ plugins/community.lua
@@ -3,12 +3,13 @@ return {
   "AstroNvim/astrocommunity",
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
-
   { import = "astrocommunity.colorscheme.catppuccin" },
   { import = "astrocommunity.colorscheme.tokyonight" },
   { import = "astrocommunity.pack.markdown" },
   { import = "astrocommunity.pack.rust" },
-  { import = "astrocommunity.editing-support.refactoring-nvim"}
-  -- { import = "astrocommunity.pack.python" },
-  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.editing-support.refactoring-nvim" },
+  { import = "astrocommunity.note-taking.obsidian-nvim" },
+  { import = "astrocommunity.motion.nvim-surround" },
+  { import = "astrocommunity.motion.hop-nvim" },
+  { import = "astrocommunity.scrolling.cinnamon-nvim" },
 }
