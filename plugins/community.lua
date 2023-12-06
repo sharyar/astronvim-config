d1f30af527844cea60d3240b98949677696cee8d
Sharyar Memon <sharyar@ualberta.ca>
2023-12-06 11:36:29 -0700

    update

--- plugins/community.lua
+++ plugins/community.lua
@@ -3,6 +3,7 @@ return {
   "AstroNvim/astrocommunity",
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
+  { import = "astrocommunity.colorscheme.onedarkpro-nvim" },
   { import = "astrocommunity.colorscheme.catppuccin" },
   { import = "astrocommunity.pack.markdown" },
   { import = "astrocommunity.pack.rust" },
